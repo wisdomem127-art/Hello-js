9540b6c1678af635ff6efcdbdfde34f6c4de92c7
Hello-js <wisdomem127@gmail.com>
2025-12-06 16:14:49 +0100

    Initial commit: Intro.js

---
+++ Intro.js
@@ -0,0 +1,7 @@
+const name = "Wisdom Emmanuel";
+const age = 22;
+const favouriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Programming Language:", favouriteLanguage);
